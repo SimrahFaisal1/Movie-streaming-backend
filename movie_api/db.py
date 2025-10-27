c3793cfa285411b9dc533e47e5e75bf88732ce5d
SimrahFaisal1 <simrahraii@gmail.com>
2025-10-27 22:37:04 +0500

    Refactor db.py by removing redundant comments
    Removed unnecessary comments to enhance code readability.

--- movie_api/db.py
+++ movie_api/db.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
 
-# Connect to your local MongoDB
 client = MongoClient("mongodb://localhost:27017")
 db = client["movieDB"]
 
-# Collections
 users_col = db["users"]
 movies_col = db["movies"]
 watch_col = db["watchHistory"]
